9dea405b6256b94b6b9bc6cb5bc6e45fc71a8189
thaherkaif <thaherkaif1404@gmail.com>
2025-04-08 12:46:50 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
+import LandingPage from './vendorDashboard/pages/LandingPage'
 import { Routes, Route } from 'react-router-dom'
 
-import LandingPage from './suby/pages/LandingPage'
-import ProductMenu from './suby/components/ProductMenu'
-import SignIn from './suby/components/forms/SignIn'
-import Register from './suby/components/forms/Register'
-
-import './App.css'
+import "./App.css"
+import NavBar from './vendorDashboard/components/NavBar'
+import Login from './vendorDashboard/components/forms/Login'
+import NotFound from './vendorDashboard/components/NotFound'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/products/:firmId/:firmName' element={<ProductMenu />} />
-        <Route path='/login' element={<SignIn />} />
-        <Route path='/signup' element={<Register />} />
+          <Route path='/' element = {<LandingPage />}/>
+          <Route path='/*' element = {<NotFound />} />
       </Routes>
     </div>
   )
